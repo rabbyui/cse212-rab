a24a5e479c3c171f9141ab93558cffc507d037d7
rabbyui <rabelcatayoc@byui.edu>
2023-11-19 19:19:59 +0800

    Human na gyud!

--- week03/code/SetsAndMapsTester.cs
+++ week03/code/SetsAndMapsTester.cs
@@ -1,43 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 
-public static class SetsAndMapsTester {
-    public static void Run() {
-        // Problem 1: Find Pairs with Sets
+public static class SetsAndMapsTester
+{
+    public static void Run()
+    {
+
+// PROBLEM 1: Find Pairs with Sets.
         Console.WriteLine("\n=========== Finding Pairs TESTS ===========");
         DisplayPairs(new[] { "am", "at", "ma", "if", "fi" });
         // ma & am
         // fi & if
+        
         Console.WriteLine("---------");
         DisplayPairs(new[] { "ab", "bc", "cd", "de", "ba" });
         // ba & ab
+        
         Console.WriteLine("---------");
         DisplayPairs(new[] { "ab", "ba", "ac", "ad", "da", "ca" });
         // ba & ab
         // da & ad
         // ca & ac
+        
         Console.WriteLine("---------");
         DisplayPairs(new[] { "ab", "ac" }); // No pairs displayed
+        
         Console.WriteLine("---------");
         DisplayPairs(new[] { "ab", "aa", "ba" });
         // ba & ab
+        
         Console.WriteLine("---------");
         DisplayPairs(new[] { "23", "84", "49", "13", "32", "46", "91", "99", "94", "31", "57", "14" });
         // 32 & 23
         // 94 & 49
         // 31 & 13
 
-        // Problem 2: Degree Summary
-        // Sample Test Cases (may not be comprehensive) 
+// PROBLEM 2: DEGREE SUMMARY
+     
         Console.WriteLine("\n=========== Census TESTS ===========");
-        Console.WriteLine(string.Join(", ", SummarizeDegrees("census.txt")));
-        // Results may be in a different order:
-        // <Dictionary>{[Bachelors, 5355], [HS-grad, 10501], [11th, 1175],
-        // [Masters, 1723], [9th, 514], [Some-college, 7291], [Assoc-acdm, 1067],
-        // [Assoc-voc, 1382], [7th-8th, 646], [Doctorate, 413], [Prof-school, 576],
-        // [5th-6th, 333], [10th, 933], [1st-4th, 168], [Preschool, 51], [12th, 433]}
+        var degreeSummary = SummarizeDegrees("D:/BYU-Idaho/FALL 2023/SECOND BLOCK/CSE 212 Programming with Data Struct/code repository/cse212-rab/week03/code/census.txt");
+        foreach (var degree in degreeSummary)
+        {
+            Console.WriteLine($"[{degree.Key}, {degree.Value}]");
+        }
 
-        // Problem 3: Anagrams
-        // Sample Test Cases (may not be comprehensive) 
+// PROBLEM 3: ANAGRAMS 
+
         Console.WriteLine("\n=========== Anagram TESTS ===========");
         Console.WriteLine(IsAnagram("CAT", "ACT")); // true
         Console.WriteLine(IsAnagram("DOG", "GOOD")); // false
@@ -50,10 +62,11 @@ public static class SetsAndMapsTester {
         Console.WriteLine(IsAnagram("Eleven plus Two", "Twelve Plus One")); // true
         Console.WriteLine(IsAnagram("Eleven plus One", "Twelve Plus One")); // false
 
-        // Problem 4: Maze
+// PROBLEM 4: MAZE
+
         Console.WriteLine("\n=========== Maze TESTS ===========");
-        Dictionary<ValueTuple<int, int>, bool[]> map = SetupMazeMap();
-        var maze = new Maze(map);
+        var mazeMap = SetupMazeMap();
+        var maze = new Maze(mazeMap);
         maze.ShowStatus(); // Should be at (1,1)
         maze.MoveUp(); // Error
         maze.MoveLeft(); // Error
@@ -75,97 +88,154 @@ public static class SetsAndMapsTester {
         maze.MoveRight();
         maze.ShowStatus(); // Should be at (6,6)
 
-        // Problem 5: Earthquake
-        // Sample Test Cases (may not be comprehensive) 
+// PROBLEM 5: EARTHQUAKE TESTS
+
         Console.WriteLine("\n=========== Earthquake TESTS ===========");
         EarthquakeDailySummary();
-
-        // Sample output from the function.  Number of earthquakes, places, and magnitudes will vary.
-        // 1km NE of Pahala, Hawaii - Mag 2.36
-        // 58km NW of Kandrian, Papua New Guinea - Mag 4.5
-        // 16km NNW of Truckee, California - Mag 0.7
-        // 9km S of Idyllwild, CA - Mag 0.25
-        // 14km SW of Searles Valley, CA - Mag 0.36
-        // 4km SW of Volcano, Hawaii - Mag 1.99
     }
 
-    /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for displaying all symmetric pairs of words.  
-    ///
-    /// For example, if <c>words</c> was: <c>[am, at, ma, if, fi]</c>, we would display:
-    /// <code>
-    /// am &amp; ma
-    /// if &amp; fi
-    /// </code>
-    /// The order of the display above does not matter. <c>at</c> would not 
-    /// be displayed because <c>ta</c> is not in the list of words.
-    ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be displayed.
-    /// </summary>
-    /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
-    private static void DisplayPairs(string[] words) {
-        // To display the pair correctly use something like:
-        // Console.WriteLine($"{word} & {pair}");
-        // Each pair of words should displayed on its own line.
+    // ===========**SOLUTIONS**=========== //
+
+// Problem #1: Find Pairs with Sets
+    private static void DisplayPairs(string[] words)
+    {
+        var wordSet = new HashSet<string>();
+        var symmetricPairs = new HashSet<string>();
+
+        foreach (var word in words)
+        {
+            var reversedWord = new string(word.Reverse().ToArray());
+
+            if (wordSet.Contains(reversedWord))
+            {
+                symmetricPairs.Add($"{word} & {reversedWord}");
+            }
+            else
+            {
+                wordSet.Add(word);
+            }
+        }
+
+        foreach (var pair in symmetricPairs)
+        {
+            Console.WriteLine(pair);
+        }
     }
 
-    /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
-    /// </summary>
-    /// <param name="filename">The name of the file to read</param>
-    /// <returns>fixed array of divisors</returns>
-    /// #############
-    /// # Problem 2 #
-    /// #############
-    private static Dictionary<string, int> SummarizeDegrees(string filename) {
+// Problem #2: Degree Summary
+    private static Dictionary<string, int> SummarizeDegrees(string filename)
+    {
         var degrees = new Dictionary<string, int>();
-        foreach (var line in File.ReadLines(filename)) {
+
+        foreach (var line in File.ReadLines(filename))
+        {
             var fields = line.Split(",");
-            // Todo Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3].Trim(); // Assuming the degree is in the 4th column
+
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
     }
 
-    /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
-    /// Examples:
-    /// is_anagram("CAT","ACT") would return true
-    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
-    /// </summary>
-    /// #############
-    /// # Problem 3 #
-    /// #############
-    private static bool IsAnagram(string word1, string word2) {
-        // Todo Problem 3 - ADD YOUR CODE HERE
-        return false;
+// Problem #3: Anagrams
+    private static bool IsAnagram(string word1, string word2)
+    {
+        var dict1 = new Dictionary<char, int>();
+        var dict2 = new Dictionary<char, int>();
+
+        foreach (var letter in word1.ToLower().Replace(" ", ""))
+        {
+            if (dict1.ContainsKey(letter))
+            {
+                dict1[letter]++;
+            }
+            else
+            {
+                dict1[letter] = 1;
+            }
+        }
+
+        foreach (var letter in word2.ToLower().Replace(" ", ""))
+        {
+            if (dict2.ContainsKey(letter))
+            {
+                dict2[letter]++;
+            }
+            else
+            {
+                dict2[letter] = 1;
+            }
+        }
+
+        return dict1.Count == dict2.Count && dict1.All(kv => dict2.TryGetValue(kv.Key, out var count) && count == kv.Value);
     }
 
-    /// <summary>
-    /// Sets up the maze dictionary for problem 4
-    /// </summary>
-    private static Dictionary<ValueTuple<int, int>, bool[]> SetupMazeMap() {
-        Dictionary<ValueTuple<int, int>, bool[]> map = new() {
+// Problem #4: Maze
+    public class Maze
+    {
+        private readonly Dictionary<ValueTuple<int, int>, bool[]> _map;
+        private ValueTuple<int, int> _currentPosition;
+
+        public Maze(Dictionary<ValueTuple<int, int>, bool[]> map)
+        {
+            _map = map;
+            _currentPosition = _map.Keys.First();
+        }
+
+        public void ShowStatus()
+        {
+            Console.WriteLine($"Current Position: {_currentPosition}");
+        }
+
+        public void MoveLeft()
+        {
+            var newPosition = (_currentPosition.Item1 - 1, _currentPosition.Item2);
+            Move(newPosition, 0);
+        }
+
+        public void MoveRight()
+        {
+            var newPosition = (_currentPosition.Item1 + 1, _currentPosition.Item2);
+            Move(newPosition, 1);
+        }
+
+        public void MoveUp()
+        {
+            var newPosition = (_currentPosition.Item1, _currentPosition.Item2 - 1);
+            Move(newPosition, 2);
+        }
+
+        public void MoveDown()
+        {
+            var newPosition = (_currentPosition.Item1, _currentPosition.Item2 + 1);
+            Move(newPosition, 3);
+        }
+
+        private void Move(ValueTuple<int, int> newPosition, int direction)
+        {
+            if (_map.TryGetValue(_currentPosition, out var validMoves) && validMoves[direction])
+            {
+                _currentPosition = newPosition;
+                Console.WriteLine($"Moved to {newPosition}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid move");
+            }
+        }
+    }
+    private static Dictionary<ValueTuple<int, int>, bool[]> SetupMazeMap()
+    {
+        Dictionary<ValueTuple<int, int>, bool[]> map = new()
+        {
             { (1, 1), new[] { false, true, false, true } },
             { (1, 2), new[] { false, true, true, false } },
             { (1, 3), new[] { false, false, false, false } },
@@ -206,22 +276,27 @@ public static class SetsAndMapsTester {
         return map;
     }
 
-    /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
-    /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will print out a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
-    /// </summary>
-    private static void EarthquakeDailySummary() {
+// Problem #5: Earthquake
+    public class Earthquake
+    {
+        public string Place { get; set; }
+        public double Magnitude { get; set; }
+    }
+
+    public class Feature
+    {
+        public Earthquake Properties { get; set; }
+    }
+
+    public class FeatureCollection
+    {
+        public List<Feature> Features { get; set; }
+    }
+
+    private static void EarthquakeDailySummary()
+    {
         const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
         using var client = new HttpClient();
         using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
         using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
@@ -230,7 +305,10 @@ public static class SetsAndMapsTester {
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
-        // 1. Add your code to map the json to the feature collection object
-        // 2. Print out each place a earthquake has happened today
+
+        foreach (var feature in featureCollection.Features)
+        {
+            Console.WriteLine($"{feature.Properties.Place} - Mag {feature.Properties.Magnitude}");
+        }
     }
 }
