a938d520bc49655bac712c51cc3f2c2846898d55
rabbyui <rabelcatayoc@byui.edu>
2023-12-08 21:55:39 +0800

    Humana jud.

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,21 +1,26 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -26,11 +31,25 @@ public class Node {
 
     public bool Contains(int value) {
         // TODO Start Problem 2
+        if (value == this.Data)
+        {
+            return true;
+        }
+        else if (value < this.Data && Left != null)
+        {
+            return Left.Contains(value);
+        }
+        else if (value > this.Data && Right != null)
+        {
+            return Right.Contains(value);
+        }
         return false;
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = (Left == null) ? 0 : Left.GetHeight();
+        int rightHeight = (Right == null) ? 0 : Right.GetHeight();
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
